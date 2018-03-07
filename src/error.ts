2fb3751cb82613b6182f4aad6b42c429e167efd3
jthomson-act <hello@jackthomson.co.uk>
2018-03-07 21:11:08 +0000

    Extract Static Method

--- src/error.ts
+++ src/error.ts
@@ -1,7 +1,11 @@
 class ErrorHandler {
-  public static handle = (error: Error) => {
+  public static handle: any;
+
+	public static __initialize_static_members() {
+		ErrorHandler.handle=(error: Error) => {
     process.stdout.write('Automate Release Webpack Plugin Error: ', error.message)
-  }
+  };
+	}
 }
 
-export { ErrorHandler }
+export { ErrorHandler };ErrorHandler.__initialize_static_members();
