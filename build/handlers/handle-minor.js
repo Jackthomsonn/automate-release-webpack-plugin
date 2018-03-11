4cf1f6f30ffc8275567d9b22f9e4a2eba59780b0
jthomson-act <hello@jackthomson.co.uk>
2018-03-11 12:28:26 +0000

    0.1.7

--- build/handlers/handle-minor.js
+++ build/handlers/handle-minor.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const version_handler_1 = require("./version-handler");
 class HandleMinor extends version_handler_1.VersionHandler {
     handleVersion() {
-        return this.pkg.version.substr(0, 2) + (Number(this.getVersionNumberToUpdate(1)) + 1) + '.' + this.pkg.version.substr(3 + 1);
+        return this.pkg.version.substr(0, 2) + (Number(this.getVersionNumberToUpdate(1)) + 1) + '.' + +'0';
     }
 }
 exports.HandleMinor = HandleMinor;
--- src/handlers/handle-minor.ts
+++ src/handlers/handle-minor.ts
@@ -2,7 +2,7 @@ import { VersionHandler } from './version-handler'
 
 class HandleMinor extends VersionHandler {
   public handleVersion(): string {
-    return this.pkg.version.substr(0, 2) + (Number(this.getVersionNumberToUpdate(1)) + 1) + '.' + this.pkg.version.substr(3 + 1)
+    return this.pkg.version.substr(0, 2) + (Number(this.getVersionNumberToUpdate(1)) + 1) + '.' + + '0'
   }
 }
 
